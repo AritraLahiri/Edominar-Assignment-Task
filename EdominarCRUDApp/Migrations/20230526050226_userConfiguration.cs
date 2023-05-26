eea4fe50af0be007598a9c65f74c012af8583f67
Aritra <aritralahiri17@gmail.com>
2023-05-26 11:04:40 +0530

    Add project files.

---
+++ EdominarCRUDApp/Migrations/20230526050226_userConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EdominarCRUDApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class userConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Address", "Email", "Gender", "HobbyId", "Mobile", "Name", "StateId" },
+                values: new object[,]
+                {
+                    { 1, "Chowrangi Road ", "das@gmail.com", 1, 4, "987456123", "Shubho Das", 28 },
+                    { 2, "61/2 Chowrashta Road", "anushka@gmail.com", 0, 5, "9874466123", "Anushka Das", 29 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
---
+++ EdominarCRUDApp/Models/Configuration/UserConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using EdominarCRUDApp.Models.db;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EdominarCRUDApp.Models.Configuration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasData(
+                 new User
+                 {
+                     Id = 1,
+                     Name = "Shubho Das",
+                     Mobile = "987456123",
+                     Email = "das@gmail.com",
+                     Gender = 1,
+                     StateId = 28,
+                     HobbyId = 4,
+                     Address = "Chowrangi Road "
+                 },
+             new User
+             {
+                 Id = 2,
+                 Name = "Anushka Das",
+                 Mobile = "9874466123",
+                 Email = "anushka@gmail.com",
+                 Gender = 0,
+                 StateId = 29,
+                 HobbyId = 5,
+                 Address = "61/2 Chowrashta Road"
+             });
+        }
+    }
+}
